e4d64a06ccd9fec6ab44dcfc91e5a0e3de30598f
pfranccino <paul.ayala02@inacapmail.cl>
2018-05-28 00:21:20 -0400

    JSONParsed

---
+++ app/src/main/java/com/example/pfranccino/mydelivery/API/JSONParser.kt
@@ -0,0 +1,28 @@
+package com.example.pfranccino.mydelivery.API
+
+import android.util.JsonToken
+import com.example.pfranccino.mydelivery.Models.Users
+import org.json.JSONObject
+
+
+class JSONParser{
+
+    fun getUser(response : JSONObject): Users {
+
+        val userJSON  = response.getJSONObject("user")
+
+        with(userJSON){
+            val user = Users(
+                             getString("first_name"),
+                             getString("last_name"),
+                             getString("email"),
+                             getString("password"),
+                             getString("password_confirmation"))
+
+                    return user
+
+
+    }
+
+    }
+    }
--- app/src/main/java/com/example/pfranccino/mydelivery/LoginActivity.kt
+++ app/src/main/java/com/example/pfranccino/mydelivery/LoginActivity.kt
@@ -2,12 +2,14 @@ package com.example.pfranccino.mydelivery
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.android.volley.AuthFailureError
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+import com.example.pfranccino.mydelivery.API.JSONParser
 import kotlinx.android.synthetic.main.activity_login.*
 import org.json.JSONObject
 
@@ -17,7 +19,7 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-
+        val jsonp = JSONParser()
         val url = "https://floating-basin-93872.herokuapp.com/api/auth/login"
         val json = JSONObject()
 
@@ -30,9 +32,12 @@ class LoginActivity : AppCompatActivity() {
 
 
             val que   = Volley.newRequestQueue(this@LoginActivity)
-            val req = object : JsonObjectRequest(Request.Method.POST,url,json, Response.Listener {
+            val req = object : JsonObjectRequest(Request.Method.POST,url,json ,Response.Listener {
 
-                response -> Toast.makeText(this,"LOGEADO",Toast.LENGTH_LONG).show()
+                response -> Toast.makeText(this,"jajaja",Toast.LENGTH_SHORT).show()
+
+
+
             },
                     Response.ErrorListener {
                         Toast.makeText(this,"CUECK",Toast.LENGTH_LONG).show()
