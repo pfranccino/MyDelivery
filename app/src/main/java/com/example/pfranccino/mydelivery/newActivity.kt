42d3e4be70c4c5fec471909f58581a36b978b07d
pfranccino <paul.ayala02@inacapmail.cl>
2018-06-01 00:41:57 -0400

    add button

--- app/src/main/java/com/example/pfranccino/mydelivery/newActivity.kt
+++ app/src/main/java/com/example/pfranccino/mydelivery/newActivity.kt
@@ -2,11 +2,17 @@ package com.example.pfranccino.mydelivery
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
 import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import android.view.View
 
 
 class newActivity : AppCompatActivity() {
+
+
+    var drawerLayout : DrawerLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,12 +20,30 @@ class newActivity : AppCompatActivity() {
 
 
 
+
         val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
 
         supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
 
     }
+
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when(item!!.itemId){
+
+            android.R.id.home ->{
+                drawerLayout!!.openDrawer(GravityCompat.START)
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
