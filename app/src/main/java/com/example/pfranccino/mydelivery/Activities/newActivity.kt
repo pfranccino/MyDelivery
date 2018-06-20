838eadeeaf56732ba8e48eac8fdb5edbdee90c5f
pfranccino <paul.ayala02@inacapmail.cl>
2018-06-20 03:29:11 -0400

    Add fragments

--- app/src/main/java/com/example/pfranccino/mydelivery/Activities/newActivity.kt
+++ app/src/main/java/com/example/pfranccino/mydelivery/Activities/newActivity.kt
@@ -3,12 +3,15 @@ package com.example.pfranccino.mydelivery.Activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.NavigationView
+import android.support.v4.app.Fragment
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.widget.Toolbar
 import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
+
+import com.example.pfranccino.mydelivery.Fragment.*
 
 import com.example.pfranccino.mydelivery.Models.User
 import com.example.pfranccino.mydelivery.R
@@ -44,6 +47,79 @@ class newActivity : AppCompatActivity() {
 
         supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        navigationView!!.setNavigationItemSelectedListener { item ->
+
+            var gestorFragment = false
+            var fragment: Fragment? = null
+
+
+            when(item.itemId){
+
+                R.id.menu_perfil ->{
+
+                    fragment = InfoFragment()
+                    gestorFragment = true
+
+                }
+                R.id.menu_compras ->{
+
+                    fragment = purchaseFragment()
+                    gestorFragment = true
+
+                }
+                R.id.menu_pagos->{
+
+                    fragment = payMethodFragment()
+                    gestorFragment = true
+
+                }
+                R.id.menu_promocion ->{
+
+                    fragment = discountFragment()
+                    gestorFragment = true
+
+                }
+                R.id.menu_factura ->{
+
+                    fragment = billFragment()
+                    gestorFragment = true
+
+                }
+                R.id.menu_ayuda ->{
+
+                    fragment = helpFragment()
+                    gestorFragment = true
+
+                }
+                R.id.menu_contacto ->{
+
+                    fragment = contactFragment()
+                    gestorFragment = true
+
+                }
+
+            }
+
+            if(gestorFragment){
+
+                changeFragment(fragment, item)
+                drawerLayout!!.closeDrawers()
+            }
+            true
+        }
+
+    }
+
+
+    fun changeFragment(fragment: Fragment? ,item: MenuItem){
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.content_frame,fragment)
+                .commit()
+
+        item.isChecked = true
+        supportActionBar!!.title = item.title
 
 
     }
--- app/src/main/java/com/example/pfranccino/mydelivery/Fragment/payMethodFragment.kt
+++ app/src/main/java/com/example/pfranccino/mydelivery/Fragment/payMethodFragment.kt
@@ -18,12 +18,12 @@ private const val ARG_PARAM2 = "param2"
  * A simple [Fragment] subclass.
  *
  */
-class ComprasFragment : Fragment() {
+class payMethodFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_compras2, container, false)
+        return inflater.inflate(R.layout.fragment_pay_method, container, false)
     }
 
 
