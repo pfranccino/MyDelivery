1f1d2406a4e48be9012cdebd26fb68804fc41c45
pfranccino <paul.ayala02@inacapmail.cl>
2018-07-04 19:04:16 -0400

    refactor view

--- app/src/main/java/com/example/pfranccino/mydelivery/Activities/SummaryOrderActivity.kt
+++ app/src/main/java/com/example/pfranccino/mydelivery/Activities/SummaryOrderActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import android.view.View
 import android.widget.ListView
 import com.example.pfranccino.mydelivery.Models.Category
@@ -72,5 +74,18 @@ class SummaryOrderActivity : AppCompatActivity() {
 
 
 
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when(item!!.itemId){
+
+            android.R.id.home ->{
+                drawerLayout!!.openDrawer(GravityCompat.START)
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
